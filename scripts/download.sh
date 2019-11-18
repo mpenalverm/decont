45be25ac8ff44e5f77df673e4d9f21b5e47edc60
Marcos Peñalver <markttes@gmail.com>
2019-11-18 18:31:59 +0100

    Modificamos scripts para descargar las muestras (sin descomprimir)

--- scripts/download.sh
+++ scripts/download.sh
@@ -1,2 +1,4 @@
 # This script should download the file specified in the first argument ($1), place it in the directory specified in the second argument, 
 # and *optionally* uncompress the downloaded file with gunzip if the third argument contains the word "yes".
+
+wget -P $2 $1
